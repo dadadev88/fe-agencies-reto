cc62c61f9bd6b825378c8ff5d08ac49cd812b277
Dhaniel Diaz <ddiazaco@everis.com>
2021-10-16 17:14:53 -0500

    Use components on pages - AgenciesModule

--- src/app/modules/agencies/agencies.module.ts
+++ src/app/modules/agencies/agencies.module.ts
@@ -5,6 +5,8 @@ import { AgenciesRoutingModule } from './agencies-routing.module';
 import { SharedModule } from 'src/app/shared/shared.module';
 
 import { AgenciesComponent } from './agencies.component';
+import { AgencyFormComponent } from './components/agency-form/agency-form.component';
+import { AgencyListItemComponent } from './components/agency-list-item/agency-list-item.component';
 import { AgenciesDetailPageComponent } from './pages/agencies-detail/agencies-detail.component';
 import { AgenciesFavoritesPageComponent } from './pages/agencies-favorites/agencies-favorites.component';
 import { AgenciesListPageComponent } from './pages/agencies-list/agencies-list.component';
@@ -15,8 +17,13 @@ const agenciesPagesComponents = [
 	AgenciesListPageComponent
 ];
 
+const agenciesComponents = [
+	AgencyListItemComponent,
+	AgencyFormComponent
+];
+
 @NgModule({
-	declarations: [ AgenciesComponent, ...agenciesPagesComponents ],
+	declarations: [ AgenciesComponent, ...agenciesComponents, ...agenciesPagesComponents ],
 	imports: [
 		CommonModule,
 		AgenciesRoutingModule,
