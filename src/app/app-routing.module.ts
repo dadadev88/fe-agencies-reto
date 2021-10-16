5ef1b47a2d30fb5ec36efc1438fedda58bcacf78
Dhaniel Diaz <ddiazaco@everis.com>
2021-10-16 16:20:17 -0500

    First commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+	{ path: '', pathMatch: 'full', redirectTo: 'agencias' },
+	{ path: 'agencias', loadChildren: () => import('./modules/agencies/agencies.module').then(m => m.AgencyModule) }
+];
+
+@NgModule({
+	imports: [ RouterModule.forRoot(appRoutes, {useHash: true}) ],
+	exports: [ RouterModule ]
+})
+export class AppRoutingModule { }
