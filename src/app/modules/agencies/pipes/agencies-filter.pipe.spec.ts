0489d2e75a90b42bb761bb1ff886bfb170214776
dadadev <dadadev88@gmail.com>
2023-05-07 21:09:18 -0500

    chore: Jenkins installing packages and run tests

--- src/app/modules/agencies/pipes/agencies-filter.pipe.spec.ts
+++ src/app/modules/agencies/pipes/agencies-filter.pipe.spec.ts
@@ -1,8 +1,30 @@
+import { AgenciesController } from '../controllers/agencies.controller';
+import { Agency } from '../interfaces/agency-list-item.interface';
 import { AgenciesFilterPipe } from './agencies-filter.pipe';
 
-describe('AgenciesFilterPipe', () => {
-  it('create an instance', () => {
-    const pipe = new AgenciesFilterPipe();
-    expect(pipe).toBeTruthy();
+describe('@AgenciesFilterPipe', () => {
+  let pipe: AgenciesFilterPipe;
+  let controllerSpy = jasmine.createSpyObj(AgenciesController, ['filterAgencies'])
+
+  beforeEach(() => {
+    pipe = new AgenciesFilterPipe(controllerSpy);
+  });
+
+  it('should filter agencies when have value to find', () => {
+    const agencies = [] as Agency[];
+    const value = 'Lima';
+
+    pipe.transform(agencies, value);
+
+    expect(controllerSpy.filterAgencies).toHaveBeenCalledWith(agencies, value);
+  });
+
+  it("should not filter agencies when don't have value to find", () => {
+    const agencies = [] as Agency[];
+    const value = '';
+
+    pipe.transform(agencies, value);
+
+    expect(controllerSpy.filterAgencies).toHaveBeenCalledTimes(0);
   });
 });
